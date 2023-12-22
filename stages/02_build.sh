9d756a412aeeb22f77ced2ccc86df2c1ec99a666
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-12-22 14:59:28 -0500

    More tuning of run

--- stages/02_build.sh
+++ stages/02_build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# Script to convert RDF .nt.gz to RDF HDT
+set -eu
+#set -o pipefail
+
+# Script to convert RDF/XML to RDF HDT
 
 # Get local path
 localpath=$(pwd)
@@ -21,16 +24,25 @@ mkdir -p $brickpath
 echo "Brick path: $brickpath"
 
 base_uri="https://www.uniprot.org/"
-mkdir $buildpath;
+
+# Set TMPDIR on same filesystem
 export TMPDIR=$buildpath/tmp
 mkdir -p $TMPDIR
+
+export buildpath_prestage=$buildpath/prestage
+mkdir -p $buildpath_prestage
+
 export buildpath brickpath base_uri
 find $downloadpath -type f -name '*.rdf.xz' | sort \
 	| parallel -J ./parallel.prf --bar '
 		RDF=$buildpath/{/.};
+		RDF_NT_GZ_TMP=$(mktemp --suffix=.nt.gz)
 		RDF_NT_GZ="$RDF".nt.gz
 		[ -f $RDF_NT_GZ ] || xz -T1 -dk < {} \
-			| $JENA_HOME/bin/riot --syntax=rdfxml --stream=ntriples --compress | sponge $RDF_NT_GZ;
-		[ -f $RDF_NT_GZ ] && rdf2hdt -i -p -B "$base_uri" $RDF_NT_GZ "$brickpath"/"$(basename "$RDF" .rdf).hdt" \
+			| $JENA_HOME/bin/riot --syntax=rdfxml --stream=ntriples | gzip > $RDF_NT_GZ_TMP \
+			&& mv -v $RDF_NT_GZ_TMP $RDF_NT_GZ;
+		[ -f $RDF_NT_GZ ] && rdf2hdt -i -p -B "$base_uri" $RDF_NT_GZ "$buildpath_prestage"/"$(basename "$RDF" .rdf).hdt" \
 			&& rm -v $RDF_NT_GZ;
 		'
+
+mv -v $buildpath_prestage/* $brickpath/
